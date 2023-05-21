195fe9f1ccb62ae8005e4cd25e1c5d732306f18c
Your Name <you@example.com>
2023-05-21 13:41:42 +0300

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# cyberbracy_mpt_original_front
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/cyberbracy_mpt_original_front/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cyberbracy_mpt_original_front
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
