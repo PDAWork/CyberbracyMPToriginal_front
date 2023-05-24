57cbff03faab85b972d9e2b9a1b66c92e6e89e1a
Your Name <you@example.com>
2023-05-24 10:53:54 +0300

    style review, add fonts, review color

--- lib/const/colors_theme.dart
+++ lib/const/colors_theme.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
 abstract final class ColorTheme {
-  static  Color red = const Color(0xFFE04142);
-  static  Color lightRed = const Color(0xFFFFDBDB);
+  static Color darkRed = const Color(0xFFE13925);
+  static  Color red = const Color(0xFFF64027);
+  static  Color lightRed = const Color(0xFFFDD6D0);
+
   static  Color grey =const  Color(0xFF848484);
   static  Color lightGrey = const Color.fromRGBO(210, 210, 219, 210);
   static Color white = const Color(0xFFEFEEF1);
