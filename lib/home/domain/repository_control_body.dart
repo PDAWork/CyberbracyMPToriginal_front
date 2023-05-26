db0226b8e1d863ff587490cd4321d42d8972c72d
PDAWork <pahomovdaniil02@yandex.ru>
2023-05-26 16:03:49 +0300

    merge chat_logic -> dev
    merge chat_logic -> dev

--- lib/home/domain/repository_control_body.dart
+++ lib/home/domain/repository_control_body.dart
@@ -1,3 +1,4 @@
+import 'package:cyberbracy_mpt_original_front/const/api_endpoints.dart';
 import 'package:cyberbracy_mpt_original_front/home/data/model/control_body_model.dart';
 import 'package:dio/dio.dart';
 
@@ -8,7 +9,7 @@ class RepositoryControlBody {
 
   Future<((bool,String), List<ControlBodyModel>)> controlBodyAll() async {
     try {
-      final result = await _dio.get('http://46.243.201.240:3077/org/list');
+      final result = await _dio.get(ApiEndpoints.controlOrganAll);
       return (
           (true,''),
         (result.data as List).map((e) => ControlBodyModel.fromJson(e)).toList()
